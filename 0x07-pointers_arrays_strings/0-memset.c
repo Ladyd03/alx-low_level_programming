dd8062bc0b43171d2bc675026e753cabfaaa85ef
ladyd03 <owumidiana@gmail.com>
2023-09-25 21:48:46 +0000

    even more pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memset - fill a block of memory with a specific value
  * @s: starting address of memory to be filled
@@ -13,9 +12,9 @@ char *_memset(char *s, char b, unsigned int n)
 	int i = 0;
 
 	for (; n > 0; i++)
-{
-	s[i] = b;
-	n--;
-
-}
+	{
+		s[i] = b;
+		n--;
+	}
 	return (s);
+}
